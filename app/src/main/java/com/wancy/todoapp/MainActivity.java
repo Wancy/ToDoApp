1d78e13581293ed631c9cbf2f3d92e63a6139db0
Wancy <flymydream2014@gmail.com>
2017-08-06 23:13:53 -0700

    Adding vcs.xml

---
+++ app/src/main/java/com/wancy/todoapp/MainActivity.java
@@ -0,0 +1,102 @@
+package com.wancy.todoapp;
+
+import android.app.Dialog;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+    ArrayList<String> todoItems;
+    ArrayAdapter<String> aToDoAdapter;
+    ListView lvItems;
+    EditText etEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        populateArrayItems();
+        lvItems = (ListView) findViewById(R.id.lvItems);
+        lvItems.setAdapter(aToDoAdapter);
+        etEditText = (EditText) findViewById(R.id.etEditText);
+        lvItems.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> adapterView, View view, int i, long l) {
+                todoItems.remove(i);
+                aToDoAdapter.notifyDataSetChanged();
+                writeItems();
+                return true;
+            }
+        });
+        lvItems.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                showEditDialog(i);
+            }
+        });
+    }
+
+    public void populateArrayItems() {
+        readItems();
+        aToDoAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, todoItems);
+    }
+
+    public void onAddItem(View view) {
+        aToDoAdapter.add(etEditText.getText().toString());
+        etEditText.setText("");
+        writeItems();
+    }
+
+    public void showEditDialog(final int index) {
+        final Dialog dialog = new Dialog(MainActivity.this);
+        dialog.setTitle("Edit Item");
+        dialog.setContentView(R.layout.edit_dialog);
+        final EditText txtEdit = (EditText) dialog.findViewById(R.id.txtEdit);
+        txtEdit.setText(todoItems.get(index));
+        Button btnEdit = (Button) dialog.findViewById(R.id.btnEdit);
+        btnEdit.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                todoItems.set(index, txtEdit.getText().toString());
+                aToDoAdapter.notifyDataSetChanged();
+                writeItems();
+                dialog.dismiss();
+            }
+        });
+        dialog.show();
+    }
+
+    private void readItems() {
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+        try {
+            if (!file.exists()) {
+                file.createNewFile();
+            }
+            todoItems = new ArrayList<String>(FileUtils.readLines(file));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void writeItems() {
+        File filesDir = getFilesDir();
+        File file = new File(filesDir, "todo.txt");
+        try {
+            FileUtils.writeLines(file, todoItems);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
